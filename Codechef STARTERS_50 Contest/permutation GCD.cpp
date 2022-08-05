bb324232f43a2a8ef3eb9be355ae88578ed73de8
amar13mehta <amar13iam@gmail.com>
2022-08-05 16:55:39 +0530

    prob code - SUMPRODSEG

---
+++ Codechef STARTERS_50 Contest/permutation GCD.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+ int t;
+ cin>>t;
+ while (t--){
+     int n,x;
+     cin>>n>>x;
+     if (x<n){
+         cout<<-1<<endl;
+         continue;
+     }
+     cout<<x-n+1<<" ";
+     for (int i=1;i<=n;i++){
+         if (i==(x-n+1) ){
+             continue;
+            
+         }
+         else
+         {
+              cout<<i<<" ";
+         }
+     }
+     cout<<endl;
+     
+     
+ }
+ return 0;
+}
